5a56c6214c44c45c38766efa14cfd01a020aad3b
Toni Junior <toni.c.junior@gmail.com>
2018-06-13 16:22:46 -0300

    COMMIT DE CRIAÇÃO DO REPOSITÓRIO

---
+++ utils/js/principal.js
@@ -0,0 +1,93 @@
+$(document).ready(function () {
+
+    $("#motionE1").hide();
+    $("#motionN1").hide();
+    $("#motionC1").hide();
+    $("#motionO1").hide();
+    $("#motionI1").hide();
+    $("#motionN2").hide();
+    $("#motionF1").hide();
+    $("#motionO2").hide();
+
+    $("#circleE1").hide();
+    $("#circleN1").hide();
+    $("#circleC1").hide();
+    $("#circleO1").hide();
+    $("#circleI1").hide();
+    $("#circleN2").hide();
+    $("#circleF1").hide();
+    $("#circleO2").hide();
+    $("image").hide();
+    $("#layer101").hide();
+
+    setTimeout(function () {
+        $("#motionE1").show();
+        $("#circleE1").show();
+        var audioElement = document.createElement('audio');
+        audioElement.setAttribute('src', 'utils/1.mp3');
+        audioElement.play();
+    }, 3000);
+    setTimeout(function () {
+        $("#motionN1").show();
+        $("#circleN1").show();
+    }, 6000);
+    setTimeout(function () {
+        $("#motionC1").show();
+        $("#circleC1").show();
+    }, 9000);
+    setTimeout(function () {
+        $("#motionO1").show();
+        $("#circleO1").show();
+    }, 12000);
+    setTimeout(function () {
+        $("#motionI1").show();
+        $("#circleI1").show();
+    }, 15000);
+    setTimeout(function () {
+        $("#motionN2").show();
+        $("#circleN2").show();
+    }, 18000);
+    setTimeout(function () {
+        $("#motionF1").show();
+        $("#circleF1").show();
+    }, 21000);
+    setTimeout(function () {
+        $("#motionO2").show();
+        $("#circleO2").show();
+    }, 24000);
+    setTimeout(function () {
+        $("#motionE1").hide();
+        $("#motionN1").hide();
+        $("#motionC1").hide();
+        $("#motionO1").hide();
+        $("#motionI1").hide();
+        $("#motionN2").hide();
+        $("#motionF1").hide();
+        $("#motionO2").hide();
+
+        $("#circleE1").hide();
+        $("#circleN1").hide();
+        $("#circleC1").hide();
+        $("#circleO1").hide();
+        $("#circleI1").hide();
+        $("#circleN2").hide();
+        $("#circleF1").hide();
+        $("#circleO2").hide();
+
+        $("image").show();
+    }, 29000);
+    setTimeout(function () {
+   
+        $("image").hide();
+        $("#layer101").show();
+
+    }, 40000);
+
+
+    $(document).keypress(function (e) {
+        if (e.which === 13) {
+            window.location.reload();
+        }
+    });
+
+});
